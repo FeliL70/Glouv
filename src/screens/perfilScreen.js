49900ae18a03e3b68cf966fd7bff43dde8e12e97
Android Studio <devandroid@ort>
2025-06-13 10:09:52 -0300

    Avances BD”

--- src/screens/perfilScreen.js
+++ src/screens/perfilScreen.js
@@ -1,12 +1,35 @@
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Image} from 'react-native';
 import { useNavigation } from '@react-navigation/native'
+import { useEffect, useState } from 'react';
 
 import Header from '../components/header';
 import BotonRojo2 from '../components/botonRojo2';
 
+import 'react-native-url-polyfill/auto'
+import 'react-native-get-random-values'
+import supabase from '../supabase';
+
 export default function perfilScreen() {
   
     const navigation = useNavigation();
+
+    const [fotoURL, setFotoURL] = useState(null);
+  
+    useEffect(() => {
+      const cargarFoto = async () => {
+    console.log({supabase})   
+     const {data} = await supabase
+          .from('Usuarios')
+          .select('fotoDePerfil')
+          .eq('id', 1)
+          .single();
+  
+          setFotoURL(data?.fotoDePerfil || null);
+      };
+  
+      cargarFoto();
+    }, []);
+
     return (
       
       <View style={{backgroundColor: '#272727', flex: 1}}>
@@ -16,6 +39,10 @@ export default function perfilScreen() {
          
                  <View style={{ height: 23 }} />
                  <Text style={styles.text}>Perfil</Text>
+
+                <Image style={{ width: 100, height: 100, borderRadius: 50}}
+                  source={{ uri: fotoURL }}/>
+
                  <View style={{ height: 123 }} />
                  <BotonRojo2 texto="Eliminar mi cuenta" onPress={() => {}}/>
                  </View>
@@ -26,8 +53,8 @@ export default function perfilScreen() {
 
   const styles = StyleSheet.create({
     perfilScreen: {
-       justifyContent: 'center',
-    alignItems: 'center',
+     justifyContent: 'center',
+     alignItems: 'center',
     },
 
     text: {
